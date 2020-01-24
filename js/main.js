0bf1c4a83180d16b0d99cd26a892989c106f1bc1
Sirius077 <57046438+Sirius077@users.noreply.github.com>
2020-01-25 00:50:04 +0500

    made up "projects" and "footer"

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".projects-slider").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        //autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
